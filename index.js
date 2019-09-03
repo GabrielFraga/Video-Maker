44f748a30ff4cddcc5e20a7f5b5e28ee7e12c377
GabrielFraga <gabrielgoog@gmail.com>
2019-09-02 22:37:14 -0300

    robo captura texto do wiki e limpa

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const readline = require('readline-sync')
+const robots = 
+{
+	text: require('./robots/text')
+}
 
-function start (){
+async function start() {
 	const content = {}
 
 	content.searchTerm = askAndReturnSearchTerm()
 	content.prefix = askAndReturnPrefix()
 
-	function askAndReturnSearchTerm(){
+	await robots.text(content)
+
+	function askAndReturnSearchTerm() {
 		return readline.question('Type a Wikipedia term: ')
 	}
 
-	function askAndReturnPrefix(){
-		const prefixes = ['Who is','What is','The history of']
+	function askAndReturnPrefix() {
+		const prefixes = ['Who is', 'What is', 'The history of']
 		const selectedPrefixIndex = readline.keyInSelect(prefixes)
 		const selectedPrefixText = prefixes[selectedPrefixIndex]
 		return selectedPrefixText
