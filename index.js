d05cacd79334942d1f2e99a09b11023ed50765b7
GabrielFraga <gabrielgoog@gmail.com>
2019-09-16 20:21:00 -0300

    first comit state

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-const readline = require('readline-sync')
-
-<<<<<<< HEAD
+const robots = {
+	input: require('./robots/input'),
+	text: require('./robots/text')
+}
 async function start() {
-	const content = {
-		maximumSentences: 7
-	}
-=======
-function start (){
-	const content = {}
->>>>>>> origin/user-input
 
-	content.searchTerm = askAndReturnSearchTerm()
-	content.prefix = askAndReturnPrefix()
-
-<<<<<<< HEAD
-	await robots.text(content)
-
-
-	function askAndReturnSearchTerm() {
-=======
-	function askAndReturnSearchTerm(){
->>>>>>> origin/user-input
-		return readline.question('Type a Wikipedia term: ')
-	}
-
-	function askAndReturnPrefix(){
-		const prefixes = ['Who is','What is','The history of']
-		const selectedPrefixIndex = readline.keyInSelect(prefixes)
-		const selectedPrefixText = prefixes[selectedPrefixIndex]
-		return selectedPrefixText
-	}
-
-	console.log(content.sentences)
+	robots.input()
+	// await robots.text(content)
 }
 start()
